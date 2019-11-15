0e513f3112eb2bc457711e96f7b7c63ac54261bb
SamTao <tth135@126.com>
2019-11-16 01:28:30 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Interpreter.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    if(argc<2){
+        std::cerr<< "No input file."<<std::endl;
+        return -1;
+    }
+    Interpreter i;
+    i.compile(argv[1]);
+    return 0;
+}
