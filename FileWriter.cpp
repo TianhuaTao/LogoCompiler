d1f7d125d4b3b2dfbea90d74d9f8e09d7cc2854a
SamTao <tth135@126.com>
2019-11-16 02:14:28 +0800

    Create FileWriter.cpp

---
+++ FileWriter.cpp
@@ -0,0 +1,13 @@
+#include "FileWriter.h"
+
+FileWriter::FileWriter()
+{
+}
+
+FileWriter::~FileWriter()
+{
+}
+
+size_t FileWriter::WriteBMP(std::string filename, const char* data, size_t size){
+    
+}
