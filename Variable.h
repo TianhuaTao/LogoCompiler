7de6bba0ac10fde5e3811e6c7acfa0120991cd00
SamTao <tth135@126.com>
2019-11-16 02:14:36 +0800

    Update Variable.h

--- Variable.h
+++ Variable.h
@@ -1,18 +1,32 @@
 #include <string>
+#include <set>
 class Variable
 {
-    typedef ValueType int;
+    friend bool operator==(const Variable &lhs, const Variable &rhs);
+    typedef int ValueType;
+
 private:
     ValueType _value;
     std::string _name;
+    const int id;
+    static int nextID;
+    static std::set<std::string, Variable*> variableSet;
 public:
-    Variable(std::string name , int initValue);
+    Variable(std::string name, int initValue);
     ~Variable();
-    ValueType getValue() const {
+    ValueType getValue() const
+    {
         return _value;
     }
-    static Variable& getVariableByName(std::string name);
-    static const Variable&  noVar();
+    static Variable &getVariableByName(std::string name);
+    static const Variable &noVar();
 };
 
-
+bool operator==(const Variable &lhs, const Variable &rhs)
+{
+    return lhs.id == rhs.id;
+}
+bool operator!=(const Variable &lhs, const Variable &rhs)
+{
+    return !(lhs == rhs);
+}
